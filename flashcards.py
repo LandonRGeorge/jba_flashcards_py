765e629b5204f948087e5840e0b3d77f56e8fcf3
LandonRGeorge <landon.robert.george@gmail.com>
2022-12-14 08:03:30 -0600

    finish stage 1

--- flashcards.py
+++ flashcards.py
@@ -1,1 +1,31 @@
-# Write your code here
+class Card:
+    def __init__(self):
+        self._front = None
+        self._back = None
+
+    @property
+    def front(self):
+        return self._front
+
+    @property
+    def back(self):
+        return self._back
+
+    def set_front(self):
+        # front = input("Card:\n")
+        print("Card:")
+        front = "2 + 2"
+        print(front)
+        self._front = front
+
+    def set_back(self):
+        # back = input("Definition:\n")
+        print("Definition:")
+        back = "4"
+        print(back)
+        self._back = back
+
+
+c = Card()
+c.set_front()
+c.set_back()
