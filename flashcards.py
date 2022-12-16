762ce77ddebe504945dca74193d01ede10edf720
LandonRGeorge <landon.robert.george@gmail.com>
2022-12-16 16:40:06 -0600

    Finish stage 3

--- flashcards.py
+++ flashcards.py
@@ -1,3 +1,6 @@
+import typing
+
+
 class Card:
     def __init__(self):
         self._front = None
@@ -11,23 +14,33 @@ class Card:
     def back(self):
         return self._back
 
-    def set_front(self):
-        front = input("Card: ")
-        self._front = front
+    @front.setter
+    def front(self, value):
+        self._front = value
 
-    def set_back(self):
-        back = input("Definition: ")
-        self._back = back
-
-    def get_answer(self):
-        answer = input("Answer: ")
-        if self.back == answer:
-            print("right")
-        else:
-            print("wrong")
+    @back.setter
+    def back(self, value):
+        self._back = value
 
 
-c = Card()
-c.set_front()
-c.set_back()
-c.get_answer()
+while True:
+    nbr_cards = input("Input the number of cards:\n")
+    try:
+        nbr_cards = int(nbr_cards)
+    except ValueError:
+        continue
+    break
+
+cards: typing.List[Card] = []
+for i in range(1, nbr_cards + 1):
+    card = Card()
+    card.front = input(f"The term for card #{i}:\n")
+    card.back = input(f"The definition for card #{i}:\n")
+    cards.append(card)
+
+for card in cards:
+    back = input(f'Print the definition of "{card.front}":\n')
+    if back == card.back:
+        print("Correct!")
+    else:
+        print(f'Wrong. The right answer is "{card.back}"')
